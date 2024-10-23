781e9961ea75cfe68982310e35f0013379a10e14
陳姵彤 <chenpeitong@chenpeitongs-MacBook-Pro.local>
2024-10-23 20:59:18 +0100

    fix(database.php): enable error reporting for development

--- auction/database.php
+++ auction/database.php
@@ -1,4 +1,8 @@
 <?php
-  $connection = mysqli_connect('localhost', 'project', 'project', 'project')
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  
+ $connection = mysqli_connect('localhost', 'prject', 'project', 'project')
   or die('Error connecting to MySQL server: ' . mysqli_connect_error());
-?> 
+?>
