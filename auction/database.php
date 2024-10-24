7114706a15bff1e08f28f8f73a642a10f666034e
陳姵彤 <chenpeitong@chenpeitongs-MacBook-Pro.local>
2024-10-24 12:10:52 +0100

    fix(database.php): correct db setting

--- auction/database.php
+++ auction/database.php
@@ -3,6 +3,6 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
   
- $connection = mysqli_connect('localhost', 'prject', 'project', 'project')
+ $connection = mysqli_connect('localhost', 'project', 'project', 'project')
   or die('Error connecting to MySQL server: ' . mysqli_connect_error());
 ?>
